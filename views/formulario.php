6f1d4289c9e6ae1c78472d7a5389c1afedc6251e
Alfred215 <alfredoarpalgmail.com>
2021-11-04 17:51:42 +0000

    son las 18:50 del 4/11

--- views/formulario.php
+++ views/formulario.php
@@ -6,11 +6,11 @@
     <body>
         <h1>Envíanos tus preguntas</h1>
         <form action="?method=guardar" method="POST"> 
-            <label>Nombre* </label><input type="text" value="" name="nombre"><br>
+            <label>Nombre* </label><input type="text" value="" name="nombre" required><br>
             <label>Apellidos </label><input type="text" value="" name="apellidos"><br>
-            <label>Correo Electronico* </label><input type="text" value="" name="correo"><br>
-            <label>Asunto* </label><input type="text" value="" name="asunto"><br>
-            <label>Mensaje* </label><input type="text" value="" name="mensaje"><br>
+            <label>Correo Electronico* </label><input type="text" value="" name="correo" required><br>
+            <label>Asunto* </label><input type="text" value="" name="asunto" required><br>
+            <label>Mensaje* </label><input type="text" value="" name="mensaje" required><br>
             <input value="Enviar" type="submit">
         </form> 
     </body>
