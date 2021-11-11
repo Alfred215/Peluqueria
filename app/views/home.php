4b5848e826639782327fca06953281634e7153e9
Alfred215 <alfredoarpalgmail.com>
2021-11-11 15:31:20 +0000

    son las 16:30 del 11/11

--- app/views/home.php
+++ app/views/home.php
@@ -6,6 +6,11 @@
     <link rel="stylesheet" href="../css/estilo.css">
 </head>
 <body>
-    
+<form method="POST">
+        <label></label><input type="text" value=""> <br>
+        <label></label><input type="text" value=""> <br>
+        <label></label><input type="text" value=""> <br>
+        <input type="submit" href="<?php PATH."/MostrarController"?>" value="enviar">
+    </form>
 </body>
 </html>
